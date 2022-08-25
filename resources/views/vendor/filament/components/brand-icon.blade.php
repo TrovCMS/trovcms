0641051b327ae50b2096acdb8ce0a099459646f2
awcodes <adam@adamwgd.com>
2022-08-25 14:04:53 -0400

    update icons and logos

---
+++ resources/views/vendor/filament/components/brand-icon.blade.php
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 188" class="text-primary-500 h-8 w-auto">
+    <title>Trov Icon</title>
+    <path fill="currentColor" d="M0 50.08V0h200v50l-46.65-6.44V25.42h-30V179.1l-46.66 8.66V25.42h-30v18.22Z"/>
+</svg>
