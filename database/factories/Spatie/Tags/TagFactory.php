5f796840c9b1a702e825af75aeff01610f0a4f71
awcodes <adam@adamwgd.com>
2022-08-25 16:04:01 -0400

    fix TagFactory

--- database/factories/Spatie/Tags/TagFactory.php
+++ database/factories/Spatie/Tags/TagFactory.php
@@ -1,14 +1,17 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\Spatie\Tags;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
 class TagFactory extends Factory
 {
+    protected $model = 'Spatie\Tags\Tag';
+
     /**
      * Define the model's default state.
      *
