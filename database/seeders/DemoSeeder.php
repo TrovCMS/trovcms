7d04c79482dc4b2a7d0f03797d6f7d488c7e4b7c
awcodes <adam@adamwgd.com>
2022-08-25 15:33:34 -0400

    add seeder tests to include modules in demo seeder

--- database/seeders/DemoSeeder.php
+++ database/seeders/DemoSeeder.php
@@ -17,5 +17,29 @@ class DemoSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(MediaSeeder::class);
         $this->call(PageSeeder::class);
+
+        if (class_exists('App\Models\Post')) {
+            $ths->call(PostSeeder::class);
+        }
+
+        if (class_exists('App\Models\Runway')) {
+            $ths->call(RunwaySeeder::class);
+        }
+
+        if (class_exists('App\Models\Faq')) {
+            $ths->call(FaqSeeder::class);
+        }
+
+        if (class_exists('App\Models\Sheet')) {
+            $ths->call(SheetSeeder::class);
+        }
+
+        if (class_exists('App\Models\DiscoveryTopic')) {
+            $ths->call(DiscoveryTopicSeeder::class);
+        }
+
+        if (class_exists('App\Models\DiscoveryArticle')) {
+            $ths->call(DiscoveryArticleSeeder::class);
+        }
     }
 }
